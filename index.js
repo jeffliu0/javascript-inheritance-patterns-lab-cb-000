8b663f2eb7f108ae72da8a62366fc9639d0f084d
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-11 20:34:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function Point(x,y) {
   this.y = y
 }
 
-Point.prototype.toString = ()=>{
-  return(`()${this.x}, ${this.y})`)
+Point.prototype.toString = function() {
+  return(`(${this.x}, ${this.y})`)
 }
 
 

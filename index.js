37e446584c8e06e9c4f6a54ffa0f825e1d7db7e9
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-11 21:16:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ Polygon.prototype.perimeter = function(){
 
 function Quadrilateral(sideOneLength, sideTwoLength, sideThreeLength, sideFourLength) {
   // call Shape constructor
-  Polygon.call(4);
+  Polygon.call(this,4);
   this.sideOneLength = sideOneLength;
   this.sideTwoLength = sideTwoLength;
   this.sideThreeLength = sideThreeLength;

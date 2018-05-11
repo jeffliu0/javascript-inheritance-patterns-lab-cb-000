643f0b4cf82fbf4ba75c5ff9c017f8674b245845
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-11 20:36:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ Point.prototype.toString = function(){
 function Shape() {}
 
 Shape.prototype.addToPlane = function(x,y){
-  this.position = Point(this,x,y)
+  this.position = new Point(x,y)
 }
 
 function Side(sides){

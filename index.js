f35922edcba8ea2c086ff8e26b7f86f3ad0f4858
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-11 18:26:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ function Shape(){
     this.position = Point.call(this,x,y)
   }
 
-  this.position = function() {
-  return(this.x + ", " + this.y);
-
   this.move = (x,y)=>{
     this.x = x
     this.y = y

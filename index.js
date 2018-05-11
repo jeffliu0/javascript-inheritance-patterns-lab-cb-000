0c317aa27bf9e3c86c5896de0ccde4a2fc5d89aa
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-11 21:09:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ Circle.prototype.diameter = function(){
   return this.radius*2
 }
 Circle.prototype.area = function(){
-  return Math.Pi*(this.radius * this.radius)
+  return Math.Pi*(this.radius ** 2)
 }
 Circle.prototype.circumference = function(){
   return Math.Pi*diameter()

1d39d83a40b2d2e1073ee019c8059fc35cd46583
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-11 19:27:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function Point(x,y) {
 
 function Shape() {
   this.addToPlane = function(x,y){
-    this.position = Point.prototype.call(this,x,y)
+    this.position = Point.call(this,x,y)
   }
   this.move = (x,y)=>{
     this.x = x
